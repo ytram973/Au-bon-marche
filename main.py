6aab8392604039f41f3c6868a1b59d3aeeb5f948
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2025-10-22 15:34:32 +0200

    ajout du typage de la classe product et ajout des fruits/légumes  avec les données fournies

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+from product import Product
+
+# ajout des fruits et légumes dans Product.products
+clementine = Product("clémentine",6,2.90,"kg","fruit")
+datte = Product("datte",4,7.00,"kg","fruit")
+grenade = Product("grenade",3,3.50,"kg","fruit")
+kaki = Product("kaki",3,4.50,"kg","fruit")
+kiwi = Product("kiwi",5,3.50,"kg","fruit")
+mandarine = Product("mandarine",6,2.80,"kg","fruit")
+orange = Product("orange",8,1.50,"kg","fruit")
+pamplemousse = Product("pamplemousse",8,2.00,"pièce","fruit")
+poire = Product("poire",5,2.50,"kg","fruit")
+pomme = Product("pomme",8,1.50,"kg","fruit")
+
+carotte = Product("carotte",7,1.30,"kg","legumes")
+choux_bruxelles = Product("choux de Bruxelles",4,4.00,"kg","legumes")
+chou_vert = Product("chou vert",12,2.50,"pièce","legumes")
+courge_butternut = Product("courge butternut",3,3.50,"pièce","legumes")
+endive = Product("endive",5,2.50,"kg","legumes")
+epinard = Product("épinard",4,2.60,"kg","legumes")
+poireau = Product("poireau",5,1.20,"kg","legumes")
+potiron = Product("potiron",6,2.50,"pièce","legumes")
+radis_noir = Product("radis_noir",10,5.00,"pièce","legumes")
+salsifis = Product("salsifis",3,2.50,"kg","legumes")
+
+
+for product in Product.products:
+    print(product)
--- product.py
+++ product.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
+from typing import ClassVar
+from dataclasses import dataclass
 
+@dataclass
 class Product:
     """
     Classe pour représenter les fruits et les légumes
     """
-    products = []
+    products : ClassVar[list['Product']] = []
+    name: str
+    stock: int
+    price: float
+    stock_unit: str
+    family: str
 
-    def __init__(self, name, price, stock_unit, stock, family):
-        self.name = name
-        self.price = price
-        self.stock_unit = stock_unit
-        self.stock = stock
-        self.family = family
+    def __post_init__(self):
         Product.products.append(self)
 
     def __repr__(self):
-        return f"{self.name} : stock -> {self.stock} {self.stock_unit}, au prix de {self.price} euros / {self.stock_unit}"
+        return f"{self.name} : stock -> {self.stock} {self.stock_unit}, prix -> {self.price} euros / {self.stock_unit}"
