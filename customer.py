d50d3707ab29075fc6d04591595da389b64b1834
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2025-10-23 10:34:18 +0200

    feat:customer ajout des fonction show_purchases et create_ticket

---
+++ customer.py
@@ -0,0 +1,21 @@
+from basket import Basket
+from product import Product
+
+
+class Customer:
+    
+    def __init__(self,nom,prenom):
+        self.nom = nom
+        self.prenom = prenom
+        self.basket = Basket()
+
+        
+    def show_purchases(self):
+        
+        self.basket.show_basket()
+        
+    def create_ticket(self):
+        
+        total = self.basket.calculate_total()
+        print(f"\nTotal to pay: {total} €")
+        
