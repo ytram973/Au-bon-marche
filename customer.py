3b93b144bc7c1653ea26783a1743d851eea14a3f
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2025-10-23 13:19:59 +0200

    doct: ajout docstring de la classe Customer

--- customer.py
+++ customer.py
@@ -1,21 +1,35 @@
 from basket import Basket
-from product import Product
+from dataclasses import dataclass
 
-
+@dataclass
 class Customer:
-    
-    def __init__(self,nom,prenom):
+    """
+    Représente un client avec un nom, un prénom et un panier d'achats.
+    """
+    def __init__(self,nom:str,prenom:str):
+        """
+        Initialise un nouveau client.
+
+        Args:
+            nom (str): Le nom du client.
+            prenom (str): Le prénom du client.
+            basket(class): la Classe basket.
+        """
         self.nom = nom
         self.prenom = prenom
         self.basket = Basket()
 
         
     def show_purchases(self):
-        
+        """
+        Fonction qui affiche le contenu du panier du client.
+        """
         self.basket.show_basket()
         
     def create_ticket(self):
-        
-        total = self.basket.calculate_total()
+        """
+        Calcule le total des achats et l'affiche le montant a payer.
+        """
+        total:float = self.basket.calculate_total()
         print(f"\nTotal to pay: {total} €")
         
