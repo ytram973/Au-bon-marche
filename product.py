8f30eb668cc5a916ffdfc844b08d0e81acdbed9c
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2025-10-23 10:04:04 +0200

    fonctions get_stock, get_price, calculate_price dans la classe Product

--- product.py
+++ product.py
@@ -10,7 +10,7 @@ class Product:
     """
     products : ClassVar[list['Product']] = []
     name: str
-    stock: int
+    stock: float
     price: float
     stock_unit: str
     family: str
@@ -21,15 +21,35 @@ class Product:
     def __repr__(self):
         return f"{self.name} : stock -> {self.stock} {self.stock_unit}, prix -> {self.price} euros / {self.stock_unit}"
 
-    def decrease_stock(self, quantity : int):
+    def decrease_stock(self, quantity : float):
         """
         Permet de mettre à jour le stock d'un produit
         :param quantity: la quantité à retirer du stock
+        :return: retourne la valeur du stock mis à jour
         """
         if quantity > self.stock:
             print(f"La quantité demandée est supérieure au stock, merci de prendre au maximum {self.stock} {self.stock_unit}")
         else:
-            self.stock = (self.stock - quantity)
+            self.stock -= quantity
             print("Stock mis à jour")
 
         return self.stock
+
+    def get_stock(self):
+        """
+        Permet de récupérer la quantité disponible (kg ou unité) d'un produit
+        """
+        return self.stock
+
+    def get_price(self):
+        """
+        Permet de récupérer le prix (kg ou unité) d'un produit
+        """
+        return self.price
+
+    def calculate_price(self, quantity: float):
+        """
+        Permet de calculer le prix d'un produit en fonction de la quantité demandée
+        """
+        total = self.price * quantity
+        return round(total, 2)
