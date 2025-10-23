32e8e32b791a65df3be9e60b58ed3d31d4b91b4a
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2025-10-23 10:36:00 +0200

    feat: basket ajout add_product show_basket calculate_total

---
+++ basket.py
@@ -0,0 +1,29 @@
+from product import Product
+
+class Basket:
+    def __init__(self):
+        self.articles = {}  
+    
+    def add_product(self, product: Product, quantity: float):
+        if product.name in self.articles:
+            self.articles[product.name][1] += quantity
+        else:
+            self.articles[product.name] = [product, quantity]
+        
+        
+        print(f"\n{quantity} {product} ajouter au panier")
+        product.decrease_stock(quantity)
+
+        
+       
+    def show_basket(self):
+        print("\nContenu du panier :")
+        for nom, (product, qte) in self.articles.items():
+            print(f"{nom} : {qte} {product.stock_unit} à {product.price} €/ {product.stock_unit}")
+
+
+    def calculate_total(self):
+        total = 0
+        for product, qty in self.articles.values():
+            total += product.calculate_price(qty)
+        return round(total, 2)
