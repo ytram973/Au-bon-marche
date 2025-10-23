34a6d342be59089de37566b657c3bfe6f70bad8e
Marty RABORD <99834828+ytram973@users.noreply.github.com>
2025-10-23 14:24:51 +0200

    feat: ajout de la Class Market avec les fonction add_client et daily_report

---
+++ market.py
@@ -0,0 +1,20 @@
+
+from customer import Customer
+
+class Market:
+    def __init__(self):
+        """Liste des clients présents dans le marché qui est ajoutée via la classe customer à sa création"""
+        self.customers = []
+
+    def add_client(self, customer:Customer):
+        """Ajoute un client à la liste des clients du marché."""
+        self.customers.append(customer)
+
+    def bilan_journalier(self):
+        """Affiche le bilan de la journée avec tous les clients et leurs achats."""
+        print("\n=== Bilan de la journée ===")
+        for customer in self.customers:
+            print(f"\nClient : {customer.nom} {customer.prenom}")
+            customer.show_purchases()
+            total = customer.basket.calculate_total()
+            print(f"Total à payer : {total} €")
